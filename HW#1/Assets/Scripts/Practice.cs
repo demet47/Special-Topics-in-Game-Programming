15f60683c76cc4c863c693e8938904de2c867567
Demet Yayla <dmtyayla@gmail.com>
2023-03-11 14:07:39 +0300

    2.6

---
+++ HW#1/Assets/Scripts/Practice.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Practice : MonoBehaviour
+{
+
+    public Vector3 force;
+    public Collider collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        force = new Vector3(0,0,-10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void FixedUpdate(){
+        this.gameObject.GetComponent<Rigidbody>().AddForce(force);
+    }
+}
